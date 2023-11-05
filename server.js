1a5f1488224f42fded83b07a8cab2888a1d60d72
Eli <eli.serra173@gmail.com>
2023-11-05 07:05:12 +0100

    Set up xFrameOptions header

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(helmet({
     }
   },
   dnsPrefetchControl: false,     // disable,
-  xFrameOptions: { action: "deny" },
+  xFrameOptions: { action: "sameorigin" },
   strictTransportSecurity: {
     maxAge: ninetyDaysInSeconds,
     includeSubDomains: true,
